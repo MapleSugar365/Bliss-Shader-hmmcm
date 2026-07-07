1d8608d0343ac95e0f033837e71a9a32d249eb27
MapleSugar365 <steady.i@foxmail.com>
2026-07-07 20:03:04 +0800

    Bliss.hmmcm.ver Release 5.6

--- shaders/lib/settings.glsl
+++ shaders/lib/settings.glsl
@@ -1,4 +1,4 @@
-#define SHADER_VERSION_LABEL 5.3 // [5.3]
+#define SHADER_VERSION_LABEL 5.6 // [5.6]
 
 #define saturate(x) clamp(x,0.0,1.0)
 
@@ -269,12 +269,12 @@ const float entityShadowDistanceMul = 0.25; // [0.01 0.02 0.03 0.04 0.05 0.10 0.
 // ----- LABPBR MATERIALS RELATED SETTINGS ----- //
 ///////////////////////////////////////////////////
 
-// #define Specular_Reflections
-// #define Screen_Space_Reflections
-// #define Sky_reflection
-// #define Rough_reflections
+#define Specular_Reflections
+#define Screen_Space_Reflections
+#define Sky_reflection
+#define Rough_reflections
 #define Dynamic_SSR_quality
-#define Sun_specular_Strength 1 // [0 1 2 3 4 5 6 7 8 9 10]
+#define Sun_specular_Strength 0 // [0 1 2 3 4 5 6 7 8 9 10]
 #define reflection_quality 30 // [6.0 7.0 8.0 9.0 10.0 11.0 12.0 13.0 14.0 15.0 16.0 17.0 18.0 19.0 20.0 25.0 30.0 35.0 40.0 45.0 50.0 55.0 60.0 65.0 70.0 75.0 80.0 85.0 90.0 95.0 100.0 ]
 #define Roughness_Threshold 1.5 // [1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3.0 ]
 
@@ -306,7 +306,7 @@ const float entityShadowDistanceMul = 0.25; // [0.01 0.02 0.03 0.04 0.05 0.10 0.
 
 // #define Porosity
 #ifdef Specular_Reflections
-	// #define Puddles // yes
+	#define Puddles // yes
 #endif
 #define Puddle_Size 1.5 // [0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5]
 
